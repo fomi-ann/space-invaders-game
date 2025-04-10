07ffa94f87cbf0661ca7692ce6330bd708612305
Anna Fomina <ann.fomi77@gmail.com>
2025-04-10 12:27:52 +0300

    add alien objects and place them on canvas

--- space.js
+++ space.js
@@ -34,6 +34,27 @@ let shipImg;
 // Ship moves one tile at a time
 let shipVelocityX = tileSize;
 
+// Aliens
+let alienArray = []; // Holds all aliens
+
+// Alien size
+let alienWidth = tileSize * 2;
+let alienHeight = tileSize;
+
+// Starting position for alien: X1, Y1
+let alienX = tileSize;
+let alienY = tileSize;
+
+let alienImg;
+
+// Number of aliens that appear in each row and each column
+let alienRows = 2;
+let alienColumns = 3;
+
+// Number for aliens to defeat
+let alienCount = 0;
+
+
 // When the page loads, the variable board will be acessing the #board tag:
 window.onload = function() {
     board = document.getElementById("board");
@@ -47,11 +68,16 @@ window.onload = function() {
 
     // Load images
     shipImg = new Image();
-    shipImg.src = "./images/ship.png"
+    shipImg.src = "./images/ship.png";
     // On load draw ship
     shipImg.onload = function(){
         context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
     }
+
+    alienImg = new Image();
+    alienImg.src = "./images/alien.png";
+
+    createAliens();
 
     requestAnimationFrame(update);
 
@@ -67,6 +93,14 @@ function update() {
 
     // Drawing ship continuesly, game loop
     context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+
+    // Aliens
+    for (let i = 0; i < alienArray.length; i++) {
+        let alien = alienArray[i];
+        if (alien.alive) {
+            context.drawImage(alienImg, alien.x, alien.y, alien.width, alien.height);
+        }
+    }
 }
 
 // e --> event 
@@ -77,4 +111,25 @@ function moveShip(e) {
     } else if (e.code == "ArrowRight" && ship.x + shipVelocityX + ship.width <= board.width) {
         ship.x += shipVelocityX; // Move ship one tile to the right
     }
+}
+
+function createAliens() {
+    // Creating 6 alien objects at the top left corner, 2 rows of 3 aliens
+    for (let c = 0; c < alienColumns; c++) {
+        for (let r = 0; r < alienRows; r++) {
+            // Alien object
+            let alien = {
+                img: alienImg,
+                x: alienX + c * alienWidth,
+                y: alienY + r * alienHeight,
+                width: alienWidth,
+                height: alienHeight,
+                alive: true
+            }
+            // add aliens to the array
+            alienArray.push(alien);
+        }
+    }
+
+    alienCount = alienArray.length;
 }
